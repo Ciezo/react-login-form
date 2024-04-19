1e9ab157ce2ae7bbbe47563b02510ad02b98d573
Cloyd Van <abcd123.letters@gmail.com>
2024-04-19 16:25:03 +0800

    Protected routes
    Implemented private or protected routes using react-auth-kit.
    I also added a fallback path to the index when the user is trying to access the private routes

--- src/App.js
+++ src/App.js
@@ -5,37 +5,59 @@ import Grid from "@mui/material/Grid";
 import LoginUser from "./pages/LoginUser";
 import RegisterUser from "./pages/RegisterUser";
 import UserHome from "./pages/UserHome";
+import AuthProvider from "react-auth-kit";
+import AuthOutlet from "@auth-kit/react-router/AuthOutlet";
+import createStore from "react-auth-kit/createStore";
 
 export default function App() {
+  /**
+   * Configure the storage to set _auth properties
+   * for cookie value assignments.
+   *
+   * Reference:
+   * https://authkit.arkadip.dev/getting_started/integration/react-app/
+   */
+  const store = createStore({
+    authName: "_auth",
+    authType: "cookie",
+    cookieDomain: window.location.hostname,
+    cookieSecure: window.location.protocol === "http:",
+  });
+
   return (
     <>
-      <Router>
-        <CssBaseline />
+      {/* This enables react-auth-kit to function as intended */}
+      <AuthProvider store={store}>    {/* https://authkit.arkadip.dev/getting_started/integration/react-app/ */}
+        <Router>
+          <CssBaseline />
 
-        <main>
           {/* Main content goes here */}
-          <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ minHeight: "100vh" }}
-          >
-            <Grid item xs={3}>
-              <Routes>
-                <Route path="/" Component={LoginUser} />
-                <Route path="/register" Component={RegisterUser} />
-                {/* Authenticated Only */}
-                {/* ========================================================== */}
-                <Route path="/home" Component={UserHome} />
-                {/* ========================================================== */}
-              </Routes>
+          <main>
+            <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              sx={{ minHeight: "100vh" }}
+            >
+              <Grid item xs={3}>
+                <Routes>
+                  <Route path="/" Component={LoginUser} />
+                  <Route path="/register" Component={RegisterUser} />
+                  {/* Authenticated Only */}
+                  {/* ========================================================== */}
+                  <Route element={<AuthOutlet fallbackPath="/" />}>
+                    <Route path="/home" Component={UserHome} />
+                  </Route>
+                  {/* ========================================================== */}
+                </Routes>
+              </Grid>
             </Grid>
-          </Grid>
-        </main>
-      </Router>
-      b
+          </main>
+
+        </Router>
+      </AuthProvider>
     </>
   );
 }
