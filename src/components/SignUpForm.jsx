d08be2fb3386791febf29ff63da864564204f11c
Cloyd Van <abcd123.letters@gmail.com>
2024-04-19 13:12:32 +0800

    POST user registration
    1. Using fetch() api
    2. Currently using a mock REST api using json-server

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -12,6 +12,9 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
 import Copyright from './Copyright';
+import Alert from '@mui/material/Alert';
+import CheckIcon from '@mui/icons-material/Check';
+import DangerousIcon from '@mui/icons-material/Dangerous';
 
 export default function SignUpForm() {
 
@@ -23,6 +26,11 @@ export default function SignUpForm() {
     password: '',
   });
 
+  // Successful user registration prompt
+  const [isSuccess, setSuccess] = React.useState(false);
+  // Error prompt
+  const [isError, setError] = React.useState(false);
+
   // Trigger onChange when form submits
   const handleChange = (event) => {
     const {name, value} = event.target;
@@ -32,13 +40,37 @@ export default function SignUpForm() {
     }));
   }
 
-  const handleSubmit = (event) => {
+
+
+  const handleSubmit = async (event) => {
     // Stop page reload
     event.preventDefault();
     /** Spread operator - iterates over an iterable object such as an array
      * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax  */
     const user = { ...formData } 
     console.log(user);
+
+    try {
+      const response = await fetch('http://localhost:8001/users', {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json' },
+        body: JSON.stringify(user) 
+      })
+      
+      if (response.ok) {
+        setSuccess(true);
+        console.log("User registered successfully!")
+      } else {
+        setError(true);
+        console.log("User registered failed!")
+        console.error("Status code: " + response.status);
+      }
+
+    } catch (error) {
+      setError(true);
+      console.log("Something wrong may have happened with sending a request to the backend!");
+      console.error(error);
+    }
   };
 
   return (
@@ -59,6 +91,18 @@ export default function SignUpForm() {
             <Typography component="h1" variant="h5">
               Sign up
             </Typography>
+
+            {/* Successful prompt when user is registered */}
+            {isSuccess && <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+              You have successfully created a user account!
+            </Alert>}
+
+            {/* Fail prompt when user cannot register */}
+            {isError && <Alert icon={<DangerousIcon fontSize="inherit" />} severity="error">
+              Unfortunately, something went wrong in creating your user account. Please, try again!
+            </Alert>
+            } 
+
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
               <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
